ffaa4b3f205017d23e6a0d1e6558174239a882a8
aimanrozali <aimanrozali@student.usm.my>
2023-01-27 05:00:33 +0800

    php composer

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -74,11 +74,11 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'dsn' => '',
+	'hostname' => '34.165.213.243',
 	'username' => 'root',
-	'password' => '',
-	'database' => 'lets_court',
+	'password' => 'i1SDtffwA9KRKa',
+	'database' => 'lets-court-db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
